0b5a9dacf4eec8a23c74f9f8ae20b227af3f681d
SUMAIYA MUSHTARI MIM <sumaiyamushtarimim@gmail.com>
2025-08-10 14:10:24 +0000

    ReferenceError: AlertDialogTrigger is not defined     at https://6000-fi

--- src/app/admin/bookings/page.tsx
+++ src/app/admin/bookings/page.tsx
@@ -21,6 +21,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { getFirestore, collection, getDocs, doc, updateDoc, deleteDoc, orderBy, query } from "firebase/firestore";
 import { getFirebaseApp } from "@/lib/firebase";
